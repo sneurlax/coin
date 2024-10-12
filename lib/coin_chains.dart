8c4724f212c8eb570aa686e10ce722e2e2f9d377
sneurlax <sneurlax@gmail.com>
2024-10-11 20:02:47 -0500

    feat: add coin_chains.dart barrel export for UTXO extensions

---
+++ lib/coin_chains.dart
@@ -0,0 +1,53 @@
+library coin_chains;
+
+export 'coin.dart';
+
+// Chain definitions
+export 'src/ext/chains/chain_params.dart';
+export 'src/ext/chains/bitcoin_params.dart';
+export 'src/ext/chains/litecoin_params.dart';
+export 'src/ext/chains/dogecoin_params.dart';
+export 'src/ext/chains/dash_params.dart';
+export 'src/ext/chains/bitcoin_cash_params.dart';
+export 'src/ext/chains/bitcoin_sv_params.dart';
+export 'src/ext/chains/bip44_paths.dart';
+export 'src/ext/chains/registry.dart';
+
+// UTXO tools
+export 'src/ext/utxo_tools/fee_estimator.dart';
+export 'src/ext/utxo_tools/coin_picker.dart';
+export 'src/ext/utxo_tools/spendable.dart';
+export 'src/ext/utxo_tools/rbf.dart';
+export 'src/ext/utxo_tools/watch_only.dart';
+export 'src/ext/utxo_tools/ordering.dart';
+
+// PSBT
+export 'src/ext/psbt/partial_tx.dart';
+export 'src/ext/psbt/partial_tx_v1.dart';
+export 'src/ext/psbt/partial_tx_v2.dart';
+export 'src/ext/psbt/psbt_codec.dart';
+export 'src/ext/psbt/psbt_signer.dart';
+export 'src/ext/psbt/psbt_types.dart';
+
+// Transaction assembly
+export 'src/ext/assembler/tx_assembler.dart';
+export 'src/ext/assembler/signing_callback.dart';
+export 'src/ext/assembler/forked_assembler.dart';
+export 'src/ext/assembler/ordering.dart';
+
+// Forked chains (BCH / BSV)
+export 'src/ext/forked_tx/forked_builder.dart';
+export 'src/ext/forked_tx/forked_hasher.dart';
+export 'src/ext/forked_tx/cash_token.dart';
+export 'src/ext/forked_tx/bcmr.dart';
+
+// Multi-signature (MuSig2)
+export 'src/ext/multi_sign/aggregated_key.dart';
+export 'src/ext/multi_sign/nonce_session.dart';
+export 'src/ext/multi_sign/partial_sig.dart';
+
+// UTXO providers
+export 'src/ext/provider/utxo_provider/electrum_link.dart';
+export 'src/ext/provider/utxo_provider/explorer_link.dart';
+export 'src/ext/provider/utxo_provider/utxo_methods.dart';
+export 'src/ext/provider/utxo_provider/utxo_models.dart';
